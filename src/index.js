61df3e22bea37f18a19809f1c5edef178751f234
dgm <vsl_sv@bk.ru>
2019-09-24 06:16:37 +0300

    feat: добавлено вычисление для двух чисел и мат. действия над ними

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    a = expr[0];
+    b = expr[2];
+
+    if (typeof expr[1] == "string") {
+// если вторым элементом знак мат. действия, то подставляем его между первым и третим элементом 
+        switch (expr[1]) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "/":
+                return a / b;
+            case "*":
+                return a * b;
+        }
+    }
 }
 
 module.exports = {
