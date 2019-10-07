6f0608c7807971009788245246f9a13c75f090fa
dgm <vsl_sv@bk.ru>
2019-10-07 07:15:54 +0300

    refactor: try create test for divizion by zero

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    a = expr[0];
+    b = expr[2];
+
+    console.log("1y=" + expr[0] + " 2y=" + expr[1] + " 3y= " + expr[2] + " 4y= " + expr[3] + " 5y= " + expr[4]);
+
+    if (typeof expr[1] == "string") {
+// если вторым элементом знак мат. действия, то подставляем его между первым и третим элементом 
+        switch (expr[1]) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "/":
+                if (expr[4] == "0") { var zero = "TypeError: Devision by zero.";
+                    return zero;
+                }
+                else {
+                return a / b;}
+            case "*":
+                return a * b;
+        }
+    }
 }
 
 module.exports = {
