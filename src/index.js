cf81df22cdb02a7acc849fc6fda8024e71b62e10
dgm <vsl_sv@bk.ru>
2019-10-08 08:59:16 +0300

    refactor: проверка на недопустимое деление

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ function expressionCalculator(expr) {
     a = expr[0];
     b = expr[2];
 
+    console.log("1y=" + expr[0] + " 2y=" + expr[1] + " 3y= " + expr[2] + " 4y= " + expr[3] + " 5y= " + expr[4]);
+
     if (typeof expr[1] == "string") {
 // если вторым элементом знак мат. действия, то подставляем его между первым и третим элементом 
         switch (expr[1]) {
@@ -15,7 +17,11 @@ function expressionCalculator(expr) {
             case "-":
                 return a - b;
             case "/":
-                return a / b;
+                if (expr[4] == "0") { var zero = "TypeError: Devision by zero.";
+                    return zero;
+                }
+                else {
+                return a / b;}
             case "*":
                 return a * b;
         }
