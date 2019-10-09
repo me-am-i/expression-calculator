4e200e416854f2134c9b1c84512006aac15113a7
dgm <vsl_sv@bk.ru>
2019-10-09 10:55:27 +0300

    feat: добавлена проверка на скобки и их количество

--- src/index.js
+++ src/index.js
@@ -11,16 +11,52 @@ function eval() {
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/TypeError
 
 
-
-
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/match
+Метод match() возвращает получившиеся совпадения при сопоставлении строки с регулярным выражением.
+пройдём по строке, и найдём количество скобок
 
 */
 
 
 
+function expressionCalculator(expr) { //на вход поступает строка String
+
+var bracketMask = /\(|\)/g; //создаём маску
+var found = expr.match(bracketMask); //ищем по маске
 
 
-function expressionCalculator(expr) {
+
+if (found.length % 2) {  //проверка количества
+        console.log(found.length + " нечетное, ошибка");
+} else {
+    console.log(found.length + " чётное, проверяем дальше");
+ }
+
+
+
+Array.isArray(array);
+
+
+console.log(found.length);
+
+var newArray = Array.from(found);
+console.log(newArray.length);
+
+
+
+//console.log(typeof array);
+//console.log(array.length);
+console.log(found);
+
+let array = Array.from(expr);  //создаём массив из входящей строки
+
+
+    //console.log(Array.isArray(array)); //проверяем, что у нас массив
+
+    //console.log(typeof expr);
+    
+
+
     a = expr[0];
     b = expr[2];
 
